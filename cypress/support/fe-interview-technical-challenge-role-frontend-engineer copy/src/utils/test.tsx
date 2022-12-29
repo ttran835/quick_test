5268cfed92b845ce17e7bea20c7c276a9bb9a93c
Tim Tran <39759466+ttran835@users.noreply.github.com>
2022-12-29 11:50:05 -0800

    Delete cypress/support/fe-interview-technical-challenge-role-frontend-engineer copy directory

--- cypress/support/fe-interview-technical-challenge-role-frontend-engineer copy/src/utils/test.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactElement } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
-
-export const renderWithProviders = (
-  ui: ReactElement,
-  opts: Partial<Parameters<typeof render>[1]> = {}
-) => render(<BrowserRouter>{ui}</BrowserRouter>, opts);
